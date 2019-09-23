79faa4c8b9288b0e44a39eccea7c3429e3cc7a5d
Hleb <velikiizevs@gmail.com>
2019-09-23 19:36:01 +0300

    add: function decode()

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = expr.split('**********'), word = [], result = [];
+    for (let i = 0; i < array.length; i ++) {
+        for (let k = 0; k < array[i].length; k+= 10) {
+        let letter = array[i].slice(k, k + 10)
+        letter = String(+letter).replace(/10/ig, '.').replace(/11/ig, '-')
+        for (key in MORSE_TABLE) {
+            if (letter === key) {
+                letter = MORSE_TABLE[key];
+            }
+        }
+        word.push(letter);
+        word
+        }
+        word = word.join('')
+        result.push(word, ' ')
+        word = []
+  
+    };
+    result = result.join('');
+    return result.slice(0, result.length - 1)
 }
 
 module.exports = {
